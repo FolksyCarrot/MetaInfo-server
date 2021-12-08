bf2c9b73e5445834dd27edfcc8990233d4523111
Keith Parks <keithparks20@gmail.com>
2021-12-08 15:54:54 -0600

    removed test folder and contents. tested API through Postman instead

--- tests/costs.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import status
-from rest_framework.test import APITestCase
-from rest_framework.authtoken.models import Token
-from metainfoapi.models import ProjectCost, Projects
-
-class CostsTests(APITestCase):
-    def setup(self):
-        
